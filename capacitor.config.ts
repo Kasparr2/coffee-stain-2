73ec7246b2d8f15c2b4172510ef2cb7796a50bce
Kasparr2 <80106401+Kasparr2@users.noreply.github.com>
2022-06-03 00:26:02 +0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Coffee Stain2',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
